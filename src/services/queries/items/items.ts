8f4d27f490fb11e7163303ffca09ab34d3a7fe52
Nick <fr13.dev@gmail.com>
2023-11-14 12:44:50 +0300

    add batching

--- src/services/queries/items/items.ts
+++ src/services/queries/items/items.ts
@@ -7,7 +7,7 @@ import { serialize } from './serialize';
 
 export const getItem = async (id: string) => {
     const item = await client.hGetAll(itemKey(id));
-    
+
     if (Object.keys(item).length === 0) {
         return null;
     }
@@ -15,12 +15,26 @@ export const getItem = async (id: string) => {
     return deserialize(id, item);
 };
 
-export const getItems = async (ids: string[]) => {};
+export const getItems = async (ids: string[]) => {
+    const commands = ids.map((id) => {
+        return client.hGetAll(itemKey(id));
+    });
+
+    const results = await Promise.all(commands);
+
+    return results.map((result, i) => {
+        if (Object.keys(result).length === 0) {
+            return null;
+        }
+        
+        return deserialize(ids[i], result);
+    })
+};
 
 export const createItem = async (attrs: CreateItemAttrs, userId: string) => {
     console.log(attrs);
 
-    const id = genId(); 
+    const id = genId();
     await client.hSet(itemKey(id), serialize(attrs));
     return id;
 };
