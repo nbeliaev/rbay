04cba7e95524866930041f0d9ee475dc846c566c
Nick <fr13.dev@gmail.com>
2023-11-23 11:35:56 +0300

    add bids

--- src/services/keys.ts
+++ src/services/keys.ts
@@ -6,6 +6,7 @@ export const itemKey = (itemId: string) => `items#${itemId}`;
 export const itemsByEndingAtKey = () => 'items:endingAt';
 export const itemsByViewsKey = () => 'items:views'; 
 export const itemsViewsKey = (itemId: string) => `items:views#${itemId}`; 
+export const bidHistoryKey = (itemId: string) => `history#${itemId}`;
 
 export const usersKey = (userId: string) => `users#${userId}`; 
 export const usernamesUniqueKey = () => 'usernames#:unique';
